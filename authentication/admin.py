5861325ce2a16c020376bd1c413450f862196c4d
lbias <306508354@qq.com>
2017-09-20 11:33:58 +0800

    settings

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
+from django.contrib import admin
+from models import SpeakerModel
 # Register your models here.
+admin.site.register(SpeakerModel)
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
+from django.shortcuts import render
+from django.http import HttpResponse
+from models import SpeakerModel
 # Create your views here.
+
+def train(request):
+    s = SpeakerModel(name= 'Peter')
+    return render(request, 'train.html', context={ 'model': s})
+
+
+def authenticate(request):
+    return HttpResponse('Authentication file page')
